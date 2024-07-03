4cd8d801b8d1f3ede9a7245baf06b12d40c0c748
Jeremiah Juinio <115476492+jrmh08@users.noreply.github.com>
2024-07-03 16:59:32 +0800

    coded hash, insert and rehash in closed hashing

--- Hashing/Hash_Closed.c
+++ Hashing/Hash_Closed.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include "Hash_Closed.h"
 
 #define MAX 10
 #define EMPTY -1
 #define DELETED -5
 
-void initDictCD(Dict *D){
+void initDictCD(CDict *D){
 	D->multiplier = 1;
 	int max = MAX * D->multiplier, x;
 	D->elems = (Word*)malloc(max * sizeof(Word));
@@ -18,7 +19,7 @@ void initDictCD(Dict *D){
 	D->count = 0;
 }
 
-Dict createDictCD(){
+CDict createDictCD(){
 	CDict D;
 	D.multiplier = 1;
 	int max = MAX * D.multiplier, x;
@@ -31,18 +32,67 @@ Dict createDictCD(){
 	return D;
 }
 
-int hashCD(int val, int multiplier){
-	
+bool isFull(CDict D){
+	return (D.count == ((MAX * D.multiplier) 0.65));
 }
 
-void insertCD(Dict *D, Word elem);
+int hashCD(int val, int multiplier){
+	return (val % (MAX * multiplier));
+}
 
-Dict reHashCD(Dict *D);
+void insertCD(CDict *D, Word elem){
+	int Hash = hashCD(elem.hashVal, D->multiplier), flag = 0;
+	if(!isFull){
+		while(flag != 1)
+		if(D->elems[Hash].data.word == EMPTY || D->elems[Hash].data.word == DELETED){
+			D->elems[Hash] = elem;
+			D->count++;
+			flag = 1;
+		}else if(Hash % (MAX * D->multiplier) > 0){
+			Hash += 1;
+		}else{
+			Hash %= MAX * D->multiplier;
+		}
+	}else{
+		reHash(D);
+		Hash = hashCD(elem.hashVal, D->multiplier);
+		while(flag != 1)
+		if(D->elems[Hash].data.word == EMPTY || D->elems[Hash].data.word == DELETED){
+			D->elems[Hash] = elem;
+			D->count++;
+			flag = 1;
+		}else if(Hash % (MAX * D->multiplier) > 0){
+			Hash += 1;
+		}else{
+			Hash %= MAX * D->multiplier;
+		}
+	}
+}
 
-int deleteWordCD(Dict *D, Word elem);
+Dict reHashCD(CDict *D){
+	CDict newDict;
+	int oldMax = D->multiplier;
+	D->multiplier++;
+	newDict.multiplier = D->multiplier;
+	int max = MAX * D->multiplier, x;
+	newDict.elems = (Word*)malloc(max * sizeof(Word));
+	for(x = 0; x < max; x++){
+		D->elems[x] = EMPTY;
+	}
+	D.count = 0;
+	
+	for(x = 0; x < oldMax; x++){
+		if(D->elems[x].word != EMPTY || D->elems[x].word != DELETED){
+			insertCD(&newDict, D->elems[x]);
+		}
+	}
+	return newDict;
+}
 
-int wordSearchCD(Dict D, Word elem);
+int deleteWordCD(CDict *D, Word elem);
 
-void displayCD(Dict D);
+int wordSearchCD(CDict D, Word elem);
 
-void visualizeCD(Dict D);
+void displayCD(CDict D);
+
+void visualizeCD(CDict D);
